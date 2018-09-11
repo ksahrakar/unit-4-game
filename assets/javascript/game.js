8a85b1ef00bb5d9b767c70c2ceb39ce5d89750c0
Kamran Sahrakar <41097862+ksahrakar@users.noreply.github.com>
2018-09-10 17:30:45 -0700

    no game loop yet - some style changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,67 +2,106 @@
 var wins = 0;
 var losses = 0;
 
+
 //Inclusive the maximum and the minimum 
 function getRandomBetween(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-$(document).ready(function() {
-  // Chose random total number in wallet (changes every game)
-  wallet = getRandomBetween(19,120);
+}
 
+$(document).ready(function() {
+  
+  // Chose random total number in wallet (changes every game)
+  var wallet = getRandomBetween(19,120);
+  var currentSpent = 0;
   // Chose number value of each gem randomly (there may be duplicates)
   var valueBlue = getRandomBetween(1,12);
-  valueRed = getRandomBetween(1,12);
+  var valueRed = getRandomBetween(1,12);
   var valuePurple = getRandomBetween(1,12);
   var valueGreen = getRandomBetween(1,12);
+  var redBought = 0;
+  var blueBought = 0;
+  var greenBought = 0;
+  var purpleBought = 0;
 
   //Show wallet total on left
-  var randomWallet = document.createElement("span");
-  $(document.getElementById("walletTotal")).append(randomWallet);
-  $(randomWallet).text(wallet);
+  $("#walletTotalH").text(wallet);
 
   // Show total purchase on right
-  currentSpent = 0;
-  function addToCurrent(a,b){
-    return a+b;
+  $("#currentTotalH").text(currentSpent);
+
+  //Function to add gem values if wallet not exceeded
+
+  function addValue(){
+    //var doneAdding = false;
+    
+      $("#redGem").on("click", function() {
+      if (currentSpent<wallet){
+        currentSpent = currentSpent+valueRed;
+        redBought++;
+        $("#currentTotalH").text(currentSpent);
+        $("#redText").text(redBought);
+        $("#messageBox").text("You bought a red gemstone");
+      } else {return}
+      });
+
+      $("#blueGem").on("click", function() {
+      if (currentSpent<wallet){  
+        currentSpent = currentSpent+valueBlue;
+        blueBought++;
+        $("#currentTotalH").text(currentSpent);
+        $("#blueText").text(blueBought);
+        $("#messageBox").text("You bought a blue gemstone");
+      } else {return}
+      });
+
+      $("#greenGem").on("click", function() {
+      if (currentSpent<wallet){
+        currentSpent = currentSpent+valueGreen;
+        greenBought++;
+        $("#currentTotalH").text(currentSpent);
+        $("#greenText").text(greenBought);
+        $("#messageBox").text("You bought a green gemstone");
+      } else {return}
+      });
+
+      $("#purpleGem").on("click", function() {
+      if (currentSpent<wallet){
+        currentSpent = currentSpent+valuePurple;
+        purpleBought++;
+        $("#currentTotalH").text(currentSpent);
+        $("#purpleText").text(purpleBought);
+        $("#messageBox").text("You bought a purple gemstone");
+      } else {return}
+      });
   }
-  var showSpent = document.createElement("span");
-  document.getElementById("spent").append(showSpent);
-  $(showSpent).text(currentSpent);
 
-  //Add gem values until reach wallet total or overspend
-  do {
-    if $((document.getElementById("redGem")).on("click") {
-    currentSpent = currentSpent+valueRed;
-    console.log(currentSpent);
-    }
-    if (document.getElementById("blueGem").click()) {
-    currentSpent = currentSpent+valueBlue;
-    console.log(currentSpent);
-    }
-    if (document.getElementById("greenGem").click()) {
-    currentSpent = currentSpent+valueGreen;
-    console.log(currentSpent);
-    }
-    if (document.getElementById("purpleGem").click()) {
-    currentSpent = currentSpent+valuePurple;
-    console.log(currentSpent);
-    }
-    $(showSpent).text(currentSpent);
-    } while (currentSpent<wallet);
-
-  if (currentSpent == wallet){
-    alert("YOU WIN!");
-    wins++;
-  } else {
-    alert("YOU OVERSPENT! You are jailed for 1 mo!")
+  function playgame (){
+    
+      while (wallet<currentSpent){
+        addValue();
+      }
   }
 
-  // $("#redGem").click(addToCurrent(currentSpent,valueRed));
+  addValue();
+  // if (currentSpent<wallet){
+  //   addValue();
+  console.log("out of loop");
+    // if (currentSpent==wallet){
+    // $("#messageBox").text("You WIN");
+    // wins++;
+    // $(".score").text("Wins: "+ wins);
+    // }
 
-  // $(showSpent).text(currentSpent);
-  
-  
-})
+    if (currentSpent>wallet){
+    $("#messageBox").text("You LOSE");
+    losses++;
+    $(".score").text("Losses: "+ losses);
+    }
+  // }
+});
+
+
+
+
