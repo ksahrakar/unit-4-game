ba3bc9e41af2c6f3d14b3f88fcb3c450cc655133
Kamran Sahrakar <41097862+ksahrakar@users.noreply.github.com>
2018-09-12 15:44:05 -0700

    added timer - not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 //Global variables
 var wins = 0;
 var losses = 0;
+var bestTime = 100;
 
 //Inclusive the maximum and the minimum 
 function getRandomBetween(min, max) {
@@ -16,10 +17,22 @@ $(document).ready(function() {
   var currentSpent = 0;
   $("#winsH").html(wins);
   $("#lossesH").html(losses);
+  var gameTime = 0;
+  var calcTime = 0;
+  var startTime;
+
+  function timer(){
+    gameTime++;
+    $("#timerH").text("Timer: " + (gameTime/10).toFixed(1));
+  }
+  
   
   //Assign value to each tag and initialize wallet
   function init(){
-    console.log("inside init");
+    if (calcTime<=bestTime){
+      bestTime = calcTime;
+    }
+    $("#bestH").text("Best time: " + bestTime);
     $("#blueGem").attr("value",getRandomBetween(1,12));
     $("#redGem").attr("value",getRandomBetween(1,12));
     $("#greenGem").attr("value",getRandomBetween(1,12));
@@ -28,21 +41,34 @@ $(document).ready(function() {
     currentSpent = 0;
     $("#walletTotalH").text(wallet);
     $("#currentTotalH").text(currentSpent);
+    $("#messageBox").text("Click a gem to buy it!");
+    gameTime = 0;
   }
 
   // Check if eligible to continue to play
   function checkit(){
-    console.log("inside checkit");
     if (currentSpent<wallet){return;}
-    if (currentSpent==wallet){
+    if (currentSpent===wallet){
+      clearInterval(gameTime);
+      calcTime = ((Date.now() - startTime)/1000).toFixed(2);
       $("#messageBox").text("You WIN!!");
       wins++;
       $("#winsH").html(wins);
+      if (confirm("Try again?")){
+        clearInterval(gameTime);
+        setTimeout(init,1500);
+        return;
+      }else{clearInterval(gameTime);return};
     }
     if (currentSpent>wallet){
       $("#messageBox").text("You overspent. IT'S JAILTIME!!");
       losses++;
       $("#lossesH").html(losses);
+      if (confirm("Try again?")){
+        clearInterval(gameTime);
+        setTimeout(init,1500);
+        return;
+      }else{clearInterval(gameTime);return};
     }
   }
   
@@ -53,19 +79,29 @@ $(document).ready(function() {
   // Show total purchase on right
   $("#currentTotalH").text(currentSpent);
 
+  // Show game timer
+  $("#timerH").text("Timer: " + (gameTime/10));
+
+  //Show best time
+  $("#bestH").text("Best time: " + bestTime);
+
+
   //Add gem values
   $(".gems").on("click", function() {
-    console.log("Inside click");
+    if (gameTime === 0){
+      gameTime = setInterval(timer,100);
+      startTime = Date.now();
+    }
     if (currentSpent<wallet){
       currentSpent = currentSpent + parseInt($(this).attr("value"));
       $("#currentTotalH").text(currentSpent);
       checkit();
       return;
     }
+    
   });
 
   init();
-  console.log("outside");
 
 });
 
